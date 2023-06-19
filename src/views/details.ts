abfb0baf6e25998f833c9d39fb6dd161fb9fe899
Vaibhav Singh <46848906+VSVAI@users.noreply.github.com>
2023-06-19 22:10:21 +0530

    routes with id

---
+++ src/views/details.ts
@@ -0,0 +1,50 @@
+const products = [
+  {
+    id: "mppt-600w",
+    name: "MPPT 600W",
+    price: 119.99,
+    rating: 4,
+    imageSrc: "/images/products/mppt-600w.jpg",
+    imageAlt: "MPPT1",
+    description:
+      "MPPT controller acts as the brain of the PV system, constantly monitoring and optimizing the power output of the solar panels. It plays a critical role in maximizing the energy harvest, improving system efficiency, and extending the battery life in solar PV installations.",
+    heading: "",
+    subHeading: "",
+    type: ["600 W", "1.0 kW"],
+    option: ["Manual", "Automatic", "MQTT"],
+    otherImages: [
+      "/images/products/mppt-600w.jpg",
+      "/images/products/mppt-600w.jpg",
+      "/images/products/mppt-600w.jpg",
+    ],
+  },
+  {
+    id: "mppt-1kw",
+    name: "MPPT 600W",
+    price: 119.99,
+    rating: 4,
+    imageSrc: "/images/products/mppt-600w.jpg",
+    imageAlt: "MPPT1",
+    description:
+      "MPPT controller acts as the brain of the PV system, constantly monitoring and optimizing the power output of the solar panels. It plays a critical role in maximizing the energy harvest, improving system efficiency, and extending the battery life in solar PV installations.",
+    heading: "",
+    subHeading: "",
+    type: ["600 W", "1.0 kW"],
+    option: ["Manual", "Automatic", "MQTT"],
+    otherImages: [
+      "/images/products/mppt-600w.jpg",
+      "/images/products/mppt-600w.jpg",
+      "/images/products/mppt-600w.jpg",
+    ],
+  },
+];
+
+const productId = window.location.pathname.split("?")[1];
+console.log(productId);
+console.log(products);
+const heading = document.getElementById("heading");
+heading!.innerHTML = products[0].name;
+
+console.log("hello");
+
+// path eventlistner
