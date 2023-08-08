b97f25f9380c8944dee38831481b8cc98979ce7d
vsvai <vsvaibhavsingh5@gmail.com>
2023-08-08 11:01:24 +0530

    robot slider

--- src/main.ts
+++ src/main.ts
@@ -92,3 +92,141 @@ const submitContactLetter = async (): Promise<void> => {
 
 (window as any).submitNewsLetter = submitNewsLetter;
 (window as any).submitContactLetter = submitContactLetter;
+
+var curpage = 1;
+var sliding = false;
+var click = true;
+var left = document.getElementById("left");
+var right = document.getElementById("right");
+var pagePrefix = "slide";
+var pageShift = 500;
+var transitionPrefix = "circle";
+var svg = true;
+
+function leftSlide() {
+  if (click) {
+    if (curpage == 1) curpage = 5;
+    console.log("woek");
+    sliding = true;
+    curpage--;
+    svg = true;
+    click = false;
+    for (let k = 1; k <= 4; k++) {
+      let a1 = document.getElementById(pagePrefix + k);
+      a1!.className += " tran";
+    }
+    setTimeout(() => {
+      move();
+    }, 200);
+    setTimeout(() => {
+      for (let k = 1; k <= 4; k++) {
+        var a1 = document.getElementById(pagePrefix + k);
+        a1!.classList.remove("tran");
+      }
+    }, 1400);
+  }
+}
+
+function rightSlide() {
+  if (click) {
+    if (curpage == 4) curpage = 0;
+    console.log("woek");
+    sliding = true;
+    curpage++;
+    svg = false;
+    click = false;
+    for (let k = 1; k <= 4; k++) {
+      var a1 = document.getElementById(pagePrefix + k);
+      a1!.className += " tran";
+    }
+    setTimeout(() => {
+      move();
+    }, 2000);
+    setTimeout(() => {
+      for (let k = 1; k <= 4; k++) {
+        var a1 = document.getElementById(pagePrefix + k);
+        a1!.classList.remove("tran");
+      }
+    }, 1400);
+  }
+}
+
+function move() {
+  if (sliding) {
+    sliding = false;
+    if (svg) {
+      for (let j = 1; j <= 9; j++) {
+        var c = document.getElementById(transitionPrefix + j);
+        c!.classList.remove("steap");
+        c!.setAttribute("class", transitionPrefix + j + " streak");
+        console.log("streak");
+      }
+    } else {
+      for (let j = 10; j <= 18; j++) {
+        var c = document.getElementById(transitionPrefix + j);
+        c!.classList.remove("steap");
+        c!.setAttribute("class", transitionPrefix + j + " streak");
+        console.log("streak");
+      }
+    }
+    setTimeout(() => {
+      for (let i = 1; i <= 4; i++) {
+        if (i == curpage) {
+          var a = document.getElementById(pagePrefix + i);
+          a!.className += " up1";
+        } else {
+          var b = document.getElementById(pagePrefix + i);
+          b!.classList.remove("up1");
+        }
+      }
+      sliding = true;
+    }, 2000);
+    setTimeout(() => {
+      click = true;
+    }, 1700);
+
+    setTimeout(() => {
+      if (svg) {
+        for (let j = 1; j <= 9; j++) {
+          var c = document.getElementById(transitionPrefix + j);
+          c!.classList.remove("streak");
+          c!.setAttribute("class", transitionPrefix + j + " steap");
+        }
+      } else {
+        for (let j = 10; j <= 18; j++) {
+          var c = document.getElementById(transitionPrefix + j);
+          c!.classList.remove("streak");
+          c!.setAttribute("class", transitionPrefix + j + " steap");
+        }
+        sliding = true;
+      }
+    }, 2000);
+    setTimeout(() => {
+      click = true;
+    }, 1700);
+  }
+}
+
+left!.onmousedown = () => {
+  leftSlide();
+};
+
+right!.onmousedown = () => {
+  rightSlide();
+};
+
+document.onkeydown = (e) => {
+  if (e.keyCode == 37) {
+    leftSlide();
+  } else if (e.keyCode == 39) {
+    rightSlide();
+  }
+};
+
+//for codepen header
+// setTimeout(() => {
+// 	rightSlide();
+// }, 500);
+setInterval(() => {
+  rightSlide();
+}, 2500);
