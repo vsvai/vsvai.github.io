a71b28370e54c6e719461893e60ebe196502f4c8
Milan Singh <milan083821@gmail.com>
2024-12-29 19:07:38 +0530

    nav color update

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,19 +15,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
-        },
-        secondary: "#31c48d"
+        primary: "#66B741",
+        secondary: "#8f8b67",
+        buttonBg: "#5d5a27",
+        text: "#2d2b1d"
       },
     },
     fontFamily: {
