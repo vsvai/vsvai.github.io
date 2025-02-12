037e1ac7cd0b824c86c34cbb6b09e56f4f73b68e
Milan Singh <milan083821@gmail.com>
2025-02-12 14:20:28 +0530

    add robot page

--- src/routes.ts
+++ src/routes.ts
@@ -11,6 +11,7 @@ import demo from "./views/demo.html?raw";
 import faq from "./views/faq.html?raw";
 import solution from "./views/solution.html?raw";
 import deletePage from "./views/deletePlaystore.html?raw";
+import robot from "./views/robot.html?raw"
 
 import "./index.css";
 
@@ -28,6 +29,7 @@ const routes: Record<string, string> = {
   "/#data": data,
   "/#demo": demo,
   "/#faq": faq,
+  "/#robot": robot,
   "/#404": data,
   "/#delete": deletePage,
 };
